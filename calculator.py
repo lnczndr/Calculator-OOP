502ff6525a1b087f4bb237ac63c615397ae15502
unknown <lanacazandra@gmail.com>
2023-05-20 15:22:30 +0800

    pseudocode

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# LANA CAZANDRA U. LEGASPI - BSCPE 1-5
+# CALCULATOR USING OOP CONCEPT
+
+# pseudocode
+# creating class "Calculator" 
+# ask for input 1
+# ask for input 2
+# ask for operation
+# define add functions
+# define subtract functions
+# define multiplication functions
+# define division function
+# add try and except/limitations
+# show output
