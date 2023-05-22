48812447623c1d5bebf5d649ef9aa9d1c7b6639d
unknown <lanacazandra@gmail.com>
2023-05-22 19:03:56 +0800

    making class Calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# LANA CAZANDRA U. LEGASPI - BSCPE 1-5
+# CALCULATOR USING OOP CONCEPT
+
+class Calculator:
+# pseudocode
+# define add functions
+    def add_input (self, inp_num1, inp_num2):
+        sum = inp_num1 + inp_num2
+        return sum
+    
+# define subtract functions
+# define multiplication functions
+# define division function
+# add try and except/limitations
+# show output               
