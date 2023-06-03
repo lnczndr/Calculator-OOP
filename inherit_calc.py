b0814e91c068404ec60ef189cfef0d4fa597ebbb
unknown <lanacazandra@gmail.com>
2023-06-04 01:21:03 +0800

    adding exponent operation

--- inherit_calc.py
+++ inherit_calc.py
@@ -10,5 +10,9 @@ class InheritCalc(Calculator):
 # inherit the calculator.py
     def __init__(self):
         super().__init__()
-        
+
 # additional operations
+
+    def exponent_input (self, inp_num1, inp_num2):
+        exponent = inp_num1 ** inp_num2
+        return exponent
